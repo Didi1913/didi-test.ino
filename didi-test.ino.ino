4af70e705ec59d064a3cbde69a8400cb51a04129
Didi <darmadi.mchydee@auk.industries>
2023-11-08 11:01:54 +0800

    another change

--- didi-test.ino.ino
+++ didi-test.ino.ino
@@ -1,6 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  // hahahaha1234
+  // hahahaha1234 asdlfkjasd;lfkjasdlf;kasjdfl;kasj
+  // hahahaha1234 asdlfkjasd;lfkjasdlf;kasjdfl;kasj
+  // hahahaha1234 asdlfkjasd;lfkjasdlf;kasjdfl;kasj
+  // hahahaha1234 asdlfkjasd;lfkjasdlf;kasjdfl;kasj
+  // hahahaha1234 asdlfkjasd;lfkjasdlf;kasjdfl;kasj
+
+
 }
 
 void loop() {
